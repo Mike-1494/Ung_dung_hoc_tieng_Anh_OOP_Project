ccba74e84d2730f7399d20c9e2559600f8c2833c
nvq2309 <nvq2309@gmail.com>
2023-12-12 06:32:15 +0700

    Update done core

--- src/App/DictionaryController.java
+++ src/App/DictionaryController.java
@@ -1,6 +1,10 @@
 package App;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -30,9 +34,13 @@ public class DictionaryController {
     @FXML
     public Text text;
     @FXML
-    private Label messagLabel;
+    private Label messageLabel;
     @FXML
-    private Button submitButton;
+    private Button submitBtn;
+    @FXML
+    private Label questionLabel;
+    @FXML
+    private GridPane multipleChoiceGrid;
 
     private final int wordLength = 5;
     private final int numRow = 6;
@@ -52,12 +60,41 @@ public class DictionaryController {
         wordleGrid.getStyleClass().clear();
         wordleGrid.setGridLinesVisible(false);
         wordleGrid.getChildren().clear();
+        submitBtn.setVisible(false);
+        messageLabel.setVisible(false);
+        questionLabel.setVisible(false);
+        multipleChoiceGrid.setVisible(false);
+        multipleChoiceGrid.getChildren().clear();
+        submitBtn.setText("Submit");
+        submitBtn.setOnAction(null);
+        submitBtn.setVisible(false);
     }
 
     public void submitAnswer(ActionEvent event) throws Exception {
+        if (submitBtn.getText() == "Try again") {
+            switchToGameWordle(event);
+            return;
+        }
         DataStore dataStore = DataStore.getInstance();
         String answer = dataStore.answer;
         int count = 0;
+        String word = "";
+        for (int i = 0; i < wordLength; i++) {
+            Node node = wordleGrid.getChildren().get(i * numRow + dataStore.currentRow);
+
+            if (node instanceof StackPane) {
+                StackPane stackPane = (StackPane) node;
+                Text text = (Text) stackPane.getChildren().get(0);
+                word += text.getText();
+            }
+        }
+
+        if (dataStore.findWord(word.toLowerCase()) == false) {
+            messageLabel.setVisible(true);
+            messageLabel.setText(word + " is not in word list! Try again.");
+            return;
+        }
+
         for (int i = 0; i < wordLength; i++) {
             Node node = wordleGrid.getChildren().get(i * numRow + dataStore.currentRow);
 
@@ -88,19 +125,25 @@ public class DictionaryController {
                 }
             }
         }
-        if (count == wordLength) {
-            mainLabel.setText("Correct!");
-        } else {
-            mainLabel.setText("Wrong!");
-        }
         dataStore.currentColumn = 0;
         dataStore.currentRow++;
+        if (count == wordLength) {
+            messageLabel.setVisible(true);
+            messageLabel.setText("Correct!");
+            submitBtn.setText("Try again");
+        } else {
+            if (dataStore.currentRow == numRow) {
+                messageLabel.setVisible(true);
+                messageLabel.setText("The answer is " + dataStore.answer + "! Try again.");
+                submitBtn.setText("Try again");
+            }
+        }
     }
 
     public void handleKeyboardInput(KeyEvent event) throws Exception {
         String key = event.getCode().toString();
         DataStore dataStore = DataStore.getInstance();
-        
+
         if (key.length() != 1) {
             if (key == "BACK_SPACE" && dataStore.state == 2 && dataStore.currentColumn > 0) {
                 Node node = wordleGrid.getChildren().get((dataStore.currentColumn - 1) * numRow + dataStore.currentRow);
@@ -125,10 +168,70 @@ public class DictionaryController {
         }
     }
 
+    private void addAnswer(String answer) {
+        StackPane stackPane = new StackPane();
+        Button button = new Button();
+        int nChildren = multipleChoiceGrid.getChildren().size();
+        char ch = (char) ('A' + nChildren);
+        button.setText(ch + ". " + answer);
+        stackPane.getChildren().add(button);
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DataStore dataStore = DataStore.getInstance();
+                String word = button.getText().split(" ")[1];
+                String answer = dataStore.answer;
+
+                if (word.toLowerCase().equals(answer.toLowerCase())) {
+                    messageLabel.setVisible(true);
+                    messageLabel.setText("Correct!");
+                    submitBtn.setText("Next question -->");
+                    submitBtn.setVisible(true);
+                    submitBtn.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent event) {
+                            try {
+                                switchToGameMultipleChoice(event);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                } else {
+                    messageLabel.setVisible(true);
+                    messageLabel.setText("Wrong answer! The answer is " + dataStore.answer);
+                }
+            }
+        });
+        multipleChoiceGrid.add(stackPane, 0, nChildren);
+    }
+
     public void switchToGameMultipleChoice(ActionEvent event) throws Exception {
         DataStore dataStore = DataStore.getInstance();
         resetScene(event);
         mainLabel.setText("Multiple Choice");
+        dataStore.wordInfo = dataStore.getWordInfo();
+        dataStore.answer = dataStore.wordInfo.word;
+        String example = dataStore.wordInfo.getExample();
+
+        dataStore.answer = example.split(" ")[(int) (Math.random() * example.split(" ").length)];
+        // remove special characters
+        dataStore.answer = dataStore.answer.replaceAll("[^a-zA-Z0-9]", "");
+
+        questionLabel.setText(example.replaceFirst(dataStore.answer, "______"));
+        questionLabel.setVisible(true);
+        String[] answers = { dataStore.getRandomWord(),
+                dataStore.answer,
+                dataStore.getRandomWord(),
+                dataStore.getRandomWord() };
+
+        Collections.shuffle(Arrays.asList(answers));
+
+        for (String answer : answers) {
+            addAnswer(answer);
+        }
+        multipleChoiceGrid.setVisible(true);
+
         dataStore.state = 1;
     }
 
@@ -136,14 +239,8 @@ public class DictionaryController {
         DataStore dataStore = DataStore.getInstance();
         resetScene(event);
         dataStore.state = 2;
-        try {
-            String randomWord = DataStore.getRandomWord(wordLength);
-            dataStore.wordInfo = DataStore.getWordInfo(randomWord);
-            dataStore.answer = dataStore.wordInfo.word;
-        } catch (Exception e) {
-            dataStore.wordInfo = DataStore.getWordInfo("hello");
-            dataStore.answer = "hello";
-        }
+        dataStore.wordInfo = dataStore.getWordInfo(wordLength);
+        dataStore.answer = dataStore.wordInfo.word;
         mainLabel.setText("Wordle");
         for (int col = 0; col < wordLength; col++)
             for (int row = 0; row < numRow; row++) {
@@ -155,6 +252,17 @@ public class DictionaryController {
             }
         // show grid border
         wordleGrid.setGridLinesVisible(true);
+        submitBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    submitAnswer(event);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        submitBtn.setVisible(true);
         // show grid
     }
 
